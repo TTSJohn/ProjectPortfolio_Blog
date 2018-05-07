01e44681c32401420139b9169463e27a718b3fcc
John Moranski <johnnymoranski21@gmail.com>
2018-05-07 13:43:59 -0500

    Attempted Authentication and added Navbar component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,36 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 import '../css/Blog.css';
 import Post from './Post'
+import Navbar from './Navbar'
+import { isLoggedIn } from '../utils/AuthService';
+
+var express = require('express');
+var app = express();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var port = process.env.PORT || 8080;
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://johnnyy.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://my_api_endpoint',
+    issuer: "https://johnnyy.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+
+app.listen(port);
 
 class Blog extends Component {
 	constructor(){
@@ -39,6 +69,7 @@ class Blog extends Component {
 
 	render(){
 		return (
+			<Navbar />
       <div className="bodyElement">
         <div className="div-board">
           <div className="row">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Blog from './components/Blog';
+import { requireAuth } from './utils/AuthService';
+import Callback from './utils/Callback';
 import registerServiceWorker from './registerServiceWorker';
+
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={Blog}/>
+        <Route path="/special" component={Blog} onEnter={requireAuth} />
+      </Router>
+    </div>
+  )
+}
 
 ReactDOM.render(<Blog />, document.getElementById('root'));
 registerServiceWorker();
